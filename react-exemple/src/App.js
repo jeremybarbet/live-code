58e9fcf4d2d0f3e8e6b872eb0180e4bc1d749e77
Jérémy Barbet <jeremgraph@gmail.com>
2018-10-23 21:54:13 +0000

    Step 1

--- react-exemple/src/App.js
+++ react-exemple/src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    isVisible: false,
+  }
+
+  onToggleOverlay = () => {
+    const { isVisible } = this.state;
+
+    this.setState({ isVisible: !isVisible });
+  }
+
   render() {
+    const { isVisible } = this.state;
+    const styles = isVisible ? "App__overlay" : "App__overlay App__overlayShow";
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p onClick={this.onToggleOverlay}>Open the overlay</p>
+
+        <div className={styles}>
+          <div className="App__block">
+            <p onClick={this.onToggleOverlay}>Close the overlay</p>
+          </div>
+
+          <div className="App__background" />
+        </div>
       </div>
     );
   }
