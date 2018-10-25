0e9d17c6444984d34e390836603ecc760f0881d0
Jérémy Barbet <jeremgraph@gmail.com>
2018-10-25 13:35:06 +0000

    Ready to start

--- react-native-exemple/App.js
+++ react-native-exemple/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={s.container}>
+
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
